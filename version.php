38286a82413d4dc7624f11524dd9c6bb78279878
FDC Wenman <139725768+FDCWenman@users.noreply.github.com>
2024-06-10 13:03:18 +0800

    Init commit

---
+++ version.php
@@ -0,0 +1,11 @@
+<?php
+# --- CSS & JS VERSION CONTROL ---
+
+// admin
+$config['admin.version'] = '2.12.13';
+
+// teacher
+$config['teacher.version'] = '2.11.94';
+
+// user
+$config['user.version'] = '9.14.62';
