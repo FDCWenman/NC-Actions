f2572103196a3fb6a005ec26a459346b40a6a595
FDC Wenman <139725768+FDCWenman@users.noreply.github.com>
2024-06-10 15:34:13 +0800

    Update version.php

--- version.php
+++ version.php
@@ -2,10 +2,10 @@
 # --- CSS & JS VERSION CONTROL ---
 
 // admin
-$config['admin.version'] = '2.12.13';
+$config['admin.version'] = '2.12.14';
 
 // teacher
-$config['teacher.version'] = '2.11.95';
+$config['teacher.version'] = '2.11.96';
 
 // user
 $config['user.version'] = '9.14.63';
